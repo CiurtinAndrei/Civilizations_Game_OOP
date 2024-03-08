2b69ab728564c5842bc872f0f83e04fc81a14592
CiurtinAndrei <149907852+CiurtinAndrei@users.noreply.github.com>
2024-03-08 15:58:42 +0200

    import

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/crism/CLionProjects/Civilizations OOP Game
+# Install script for directory: C:/Users/crism/Documents/GitHub/Civilizations_Game_OOP
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/crism/CLionProjects/Civilizations OOP Game/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/crism/Documents/GitHub/Civilizations_Game_OOP/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
